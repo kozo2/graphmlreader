504a4bbe84971703695b87d1b26a547dc55b973b
Kozo Nishida <knishida@riken.jp>
2010-10-27 16:53:57 +0000

    added new archetype

---
+++ graphmlreader/src/main/java/org/cytoscape/App.java
@@ -0,0 +1,13 @@
+package org.cytoscape;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
